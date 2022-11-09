25fc116aa20fc34b259669385f647daca95ab61c
Akash <na.akash715@gmail.com>
2022-11-10 05:39:21 +0600

    Jwt token implemented

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ const jwt = require('jsonwebtoken');
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    // console.log(req.headers.authorization);
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access!' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (error, decoded) {
+        if (error) {
+            return res.status(401).send({ message: 'unauthorized access!' });
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
+
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mktejfv.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -40,8 +58,16 @@ async function run() {
         });
 
 
-        //getting email wise reviews
-        app.get('/reviews', async (req, res) => {
+        //getting email wise reviews and using jwt
+        app.get('/reviews', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            // console.log(decoded);
+
+            if (decoded.email !== req.query.email) {
+                res.status(403).send({ message: 'Forbidden Access!' });
+            }
+
+
             const query = {
                 email: req.query.email
             };
@@ -49,6 +75,7 @@ async function run() {
             const reviews = await cursor.toArray();
             res.send(reviews);
         });
+
 
         //getting a specific review
         app.get('/update/:id', async (req, res) => {
@@ -118,6 +145,13 @@ async function run() {
         });
 
         ///services end
+
+        //jwt token
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" });
+            res.send({ token });
+        });
 
 
 
